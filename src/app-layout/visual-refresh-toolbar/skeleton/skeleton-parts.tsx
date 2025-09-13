2614e6c31519f257711ffe95c7f7216f515bccdc
Trevor Burnham <trevorburnham@gmail.com>
2025-09-13 10:52:51 -0400

    fix: Fix AppLayoutToolbar breadcrumbs SSR glitch

--- src/app-layout/visual-refresh-toolbar/skeleton/skeleton-parts.tsx
+++ src/app-layout/visual-refresh-toolbar/skeleton/skeleton-parts.tsx
@@ -1,12 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
+import clsx from 'clsx';
 
 import { AppLayoutNotificationsImplementationProps } from '../notifications';
 import { AppLayoutToolbarImplementationProps } from '../toolbar';
 import { SkeletonPartProps } from './interfaces';
 import { BreadcrumbsSlot, NotificationsSlot, ToolbarSlot } from './slots';
 
+import testutilStyles from '../../test-classes/styles.css.js';
+import toolbarStyles from '../toolbar/styles.css.js';
 import styles from './styles.css.js';
 
 /**
@@ -19,7 +22,12 @@ export const BeforeMainSlotSkeleton = React.forwardRef<HTMLElement, SkeletonPart
       <>
         {!!toolbarProps && (
           <ToolbarSlot ref={ref}>
-            <BreadcrumbsSlot ownBreadcrumbs={appLayoutProps.breadcrumbs} />
+            <div className={toolbarStyles['toolbar-container']}>
+              <div className={clsx(toolbarStyles['universal-toolbar-breadcrumbs'], testutilStyles.breadcrumbs)}>
+                <BreadcrumbsSlot ownBreadcrumbs={appLayoutProps.breadcrumbs} />
+              </div>
+              <div className={toolbarStyles['universal-toolbar-drawers']} />
+            </div>
           </ToolbarSlot>
         )}
         {toolbarProps?.navigationOpen && <div className={styles.navigation} />}
@@ -35,14 +43,19 @@ export const BeforeMainSlotSkeleton = React.forwardRef<HTMLElement, SkeletonPart
 export const ToolbarSkeleton = React.forwardRef<HTMLElement, AppLayoutToolbarImplementationProps>(
   ({ appLayoutInternals }: AppLayoutToolbarImplementationProps, ref) => (
     <ToolbarSlot ref={ref}>
-      <BreadcrumbsSlot
-        ownBreadcrumbs={appLayoutInternals.breadcrumbs}
-        discoveredBreadcrumbs={appLayoutInternals.discoveredBreadcrumbs}
-      />
+      <div className={toolbarStyles['toolbar-container']}>
+        <div className={clsx(toolbarStyles['universal-toolbar-breadcrumbs'], testutilStyles.breadcrumbs)}>
+          <BreadcrumbsSlot
+            ownBreadcrumbs={appLayoutInternals.breadcrumbs}
+            discoveredBreadcrumbs={appLayoutInternals.discoveredBreadcrumbs}
+          />
+        </div>
+        <div className={toolbarStyles['universal-toolbar-drawers']} />
+      </div>
     </ToolbarSlot>
   )
 );
 
 export const NotificationsSkeleton = React.forwardRef<HTMLElement, AppLayoutNotificationsImplementationProps>(
-  (props: AppLayoutNotificationsImplementationProps, ref) => <NotificationsSlot ref={ref} />
+  (_props: AppLayoutNotificationsImplementationProps, ref) => <NotificationsSlot ref={ref} />
 );
